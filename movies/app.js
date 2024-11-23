19dd6646f25942c0665d16eb49734317097982c8
physiodevapp <edu.gamboa.rodriguez@gmail.com>
2024-11-23 13:55:27 +0100

    feat: add CORS config

--- movies/app.js
+++ movies/app.js
@@ -2,9 +2,24 @@ const express = require('express')
 const movies = require('./movies.json')
 const crypto = require('node:crypto')
 const { validateMovie, validateUpdateMovie } = require('./schemas/movieSchema')
+const cors = require('cors')
 
 const app = express()
 app.use(express.json())
+app.use(cors({
+  origin: (origin, callback) => {
+    const ACCEPTED_ORIGINS = [
+      'http://localhost:8080',
+      'http://localhost:1234'
+    ]
+
+    if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
+      return callback(null, true)
+    }
+
+    return callback(new Error('Not allowed by CORS'))
+  }
+}))
 
 app.disable('x-powered-by')
 
