b37fdcd8a8411f76345ed44e33e1e2ed15bff218
physiodevapp <edu.gamboa.rodriguez@gmail.com>
2024-11-23 12:54:43 +0100

    feat: create basic API REST

--- films/app.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-
-const app = express()
-
-const PORT = process.env.PORT ?? 1234
-
-app.listen(PORT, () => {
-  console.log(`Server listening at port: ${PORT}`);
-})
---
+++ movies/app.js
@@ -0,0 +1,97 @@
+const express = require('express')
+const movies = require('./movies.json')
+const crypto = require('node:crypto')
+const { validateMovie, validateUpdateMovie } = require('./schemas/movieSchema')
+
+const app = express()
+app.use(express.json())
+
+app.disable('x-powered-by')
+
+const PORT = process.env.PORT ?? 1234
+
+app.get('/movies', (req, res) => {
+  const { genre } = req.query
+
+  if (genre) {
+    const genreMovies = movies.filter((movie) => movie.genre.some((movieGenre) => movieGenre.toLowerCase() === genre.toLowerCase()))
+
+    return res.status(200).json(genreMovies)
+  }
+
+  res.status(200).json(movies)
+})
+
+app.get('/movies/:id', (req, res) => {
+  const { id } = req.params
+
+  const movieIndex = movies.findIndex((movie) => movie.id === id)
+
+  if (movieIndex < 0) {
+    return res.status(404).send('Movie not found')
+  }
+
+  res.status(200).send(movies[movieIndex])
+})
+
+app.post('/movies', (req, res) => {
+  const result = validateMovie(req.body)
+
+  if (!result.success) {
+    return res.status(400).json({ error: JSON.parse(result.error) })
+  }
+
+  const newMovie = {
+    id: crypto.randomUUID(),
+    ...result.data
+  }
+
+  movies.push(newMovie)
+
+  res.status(201).json(newMovie)
+})
+
+app.patch('/movies/:id', (req, res) => {
+  const { id } = req.params
+  const movieIndex = movies.findIndex((movie) => movie.id === id)
+
+  if (movieIndex < 0) {
+    return res.status(404).send('Movie not found')
+  }
+
+  const result = validateUpdateMovie(req.body)
+
+  if (result.error) {
+    return res.status(404).json({ error: JSON.parse(result.error.message) })
+  }
+
+  const updateMovie = {
+    ...movies[movieIndex],
+    ...result.data
+  }
+
+  movies[movieIndex] = updateMovie
+
+  res.status(200).json(updateMovie)
+})
+
+app.delete('/movies/:id', (req, res) => {
+  const { id } = req.params
+  const movieIndex = movies.findIndex((movie) => movie.id === id)
+
+  if (movieIndex < 0) {
+    return res.status(404).send('Movie not found')
+  }
+
+  movies.splice(movieIndex, 1)
+
+  res.status(200).json({ message: 'Movie deleted' })
+})
+
+app.use((req, res, error) => {
+  res.status(404).send('Not found')
+})
+
+app.listen(PORT, () => {
+  console.log(`Server listening at port: ${PORT}`)
+})
