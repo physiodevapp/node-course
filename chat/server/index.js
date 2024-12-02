b22d4ecfe95d04ac5a72c10b0a8d380f85cfbe2c
physiodevapp <edu.gamboa.rodriguez@gmail.com>
2024-12-02 11:50:35 +0100

    create io and socket listeners

--- chat/server/index.js
+++ chat/server/index.js
@@ -1,16 +1,38 @@
 import express from 'express'
 import logger from 'morgan'
 
+import { Server } from 'socket.io'
+import { createServer } from 'node:http'
+
+
 const port = process.env.PORT ?? 3000
 
 const app = express()
+const server = createServer(app)
+const io = new Server(server, {
+  connectionStateRecovery: {
+    maxDisconnectionDuration: 10000
+  }
+})
+
+io.on('connection', (socket) => {
+  console.log('An user has connected');
+
+  socket.on('disconnect', () => {
+    console.log('An user has disconnected');
+  })
+
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg)
+  })
+})
 
 app.use(logger('dev'))
 
-app.get('/', (req, res) => {
-  res.send('<h1>Esto es el servidor</h1>')
+app.get('/',  (req, res) => {
+  res.sendFile(process.cwd() + '/client/index.html')
 })
 
-app.listen(port, () => {
-  console.log(`Server running on port${port}`)
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`)
 })
