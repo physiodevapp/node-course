722453a772d5c69a22725080e81805300963aa66
physiodevapp <edu.gamboa.rodriguez@gmail.com>
2024-12-04 10:27:03 +0100

    feat: create index.html

--- authentication/index.js
+++ authentication/index.js
@@ -2,9 +2,14 @@ import express from 'express'
 import { PORT } from './env-config.js';
 
 const app = express()
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('hola mundo')
+})
+
+app.get('/protected', (req, res) => {
+  res.send('This content is protected')
 })
 
 app.listen(PORT, () => {
